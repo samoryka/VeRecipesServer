cf215994bbdbeda8005f431c69911ff3c7719d34
Samory Ka <ka.samory@gmail.com>
2017-03-07 12:20:12 +0100

    Authentication correction : login shouldn't require authentication

--- VeRecipesDeployableServer/src/main/java/com/samoryka/verecipesdeployableserver/Security/AuthenticationConfiguration.java
+++ VeRecipesDeployableServer/src/main/java/com/samoryka/verecipesdeployableserver/Security/AuthenticationConfiguration.java
@@ -63,7 +63,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
             .antMatchers("/swagger-ui.html").permitAll()    // any person can view the documentation
-            .antMatchers(HttpMethod.PUT,"/user").permitAll()    // any person can sign up
+            .antMatchers("/user").permitAll()    // any person can sign up or login
             .antMatchers(HttpMethod.PUT,"/recipe").hasRole("ADMIN") // only an admin can add a recipe
             .anyRequest().fullyAuthenticated()
             .and().formLogin().loginPage("/").permitAll()
